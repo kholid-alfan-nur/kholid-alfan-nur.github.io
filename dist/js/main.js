1e1cf2168ec38a6325b6efc4f9e4694566e0147d
Kholid Alfan Nur <kholidnur85@gmail.com>
2019-02-06 17:17:48 -0500

    added menu selectors in main.js

---
+++ dist/js/main.js
@@ -0,0 +1,8 @@
+// select dom items
+const menuBtn = document.querySelector('.btn-menu');
+const menu = document.querySelector('.menu');
+const menuNav = document.querySelector('.menu-nav');
+const menuBranding = document.querySelector('.menu-branding');
+const navItems = document.querySelectorAll('.nav-items');
+
+// set initial state of menu
